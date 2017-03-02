c96fd40698f7df1c2d45477ffd0dd6d02e709fe3
iams3cond <andr3whunter@gmail.com>
2017-03-02 13:40:52 -0500

    Seems complete. May deploy to heroku

--- Catalog/models.py
+++ Catalog/models.py
@@ -2,12 +2,14 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.forms import ModelForm
+from django import forms
+
 
 
 # Create your models here.
 DURATION_CHOICES = (
-    ('8', '8'),
-    ('2', '2')
+    ('2', '2'),
+    ('8', '8')
 )
 
 class Course(models.Model):
@@ -21,3 +23,10 @@ class Course_index(ModelForm):
     class Meta:
         model = Course
         fields = ['Title', 'Description', 'Instructor', 'Duration', 'CourseArt']
+        widgets = {
+            'Title': forms.TextInput(attrs={'placeholder': 'Name it something good'}),
+            'Description': forms.TextInput(attrs={'placeholder': 'Provide some information about what students will learn'}),
+            'Instructor': forms.TextInput(attrs={'placeholder': 'Django Guru'}),
+
+
+        }
