4e7dba2d46d0a4d10b661ea82792ac0a5625254a
iams3cond <andr3whunter@gmail.com>
2017-03-01 22:14:05 -0500

    Edited Models

--- Catalog/models.py
+++ Catalog/models.py
@@ -7,17 +7,17 @@ from django.forms import ModelForm
 # Create your models here.
 DURATION_CHOICES = (
     ('8', '8'),
-    ('12', '12')
+    ('2', '2')
 )
 
 class Course(models.Model):
     Title = models.CharField(max_length=200)
     Description = models.TextField(max_length = 500)
     Instructor = models.CharField(max_length = 100)
-    Duration = models.CharField(max_length = 2, choices = DURATION_CHOICES, default = '8')
-    #CourseArt = models.FileField(upload_to='uploads/')
+    Duration = models.CharField(max_length = 1, choices = DURATION_CHOICES, default = '8')
+    CourseArt = models.FileField(upload_to='uploads/')
     
 class Course_index(ModelForm):
     class Meta:
         model = Course
-        fields = ['Title', 'Description', 'Instructor', 'Duration']
+        fields = ['Title', 'Description', 'Instructor', 'Duration', 'CourseArt']
